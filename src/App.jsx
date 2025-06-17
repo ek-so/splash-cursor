753f7cc2d060ed15d2c195395d6c5a111bfd97f9
ek-so <eksomail@gmail.com>
2025-06-17 09:40:12 +0200

    New app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import SplashCursor from './splashCursor'
+
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh', background: 'black' }}>
+      <SplashCursor />
+    </div>
+  )
+}
+
+export default App
